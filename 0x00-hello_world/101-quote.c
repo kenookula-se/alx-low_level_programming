fe324b91bf25a0265ea248ca0613885002c95763
Keno Okula <kenookula@gmail.com>
2023-03-17 06:11:45 -0700

    Commit 10.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,6 @@
 int main(void)
 {
     char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, 59);  // 59 is the length of the message
+    write(2, msg, 59);
     return 1;
 }
