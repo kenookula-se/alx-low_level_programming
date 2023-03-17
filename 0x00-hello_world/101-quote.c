6529f762add582038ced3289acc9fde55ac63030
Keno Okula <kenookula@gmail.com>
2023-03-17 06:17:02 -0700

    Commit 10.5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,7 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <unistd.h>
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+    char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, msg, 59); 
+    return 1;
 }
